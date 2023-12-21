f35ad8f61da8b576130e7b2f3077dc64f1a4e5da
SammyBoyDeluxe <151546643+SammyBoyDeluxe@users.noreply.github.com>
2023-12-21 19:41:37 +0100

    Revert "Fixed MelodyTrack generation"
    This reverts commit effea28019dbbd6e28c0edaeb26a6f3c33af1955.

--- src/main/java/com/example/midistouch/HelloApplication.java
+++ src/main/java/com/example/midistouch/HelloApplication.java
@@ -6,11 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class HelloApplication extends Application {
-
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -22,8 +19,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-       Track[] tracks = TrackGenerator.generateTracks((byte)60, (byte)0,(byte)16);
-        System.out.println(Arrays.deepToString(tracks));
-
     }
 }
